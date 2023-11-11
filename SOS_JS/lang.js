0128d4ad0e403d8c50869c99647de4e3a619f991
r-german <111256133+r-german@users.noreply.github.com>
2023-11-11 23:14:32 +0300

    Add files via upload

---
+++ SOS_JS/lang.js
@@ -0,0 +1,307 @@
+const textObject = {
+    "attr-name1": {
+        "en": "about",
+        "ru": "об игре",
+        "be": "аб гульні"
+    },
+    "attr-name2": {
+        "en": "game features",
+        "ru": "особенности",
+        "be": "асаблівасці"
+    },
+    "attr-name3": {
+        "en": "system requirements",
+        "ru": "сис. требования",
+        "be": "сіс. патрабаванні"
+    },
+    "attr-name4": {
+        "en": "quotes",
+        "ru": "комментарии",
+        "be": "каментар"
+    },
+    "attr-name5": {
+        "en": "survive at all costs",
+        "ru": "выживи любой ценой",
+        "be": "выжыві любой цаной"
+    },
+    "attr-name6": {
+        "en": "experience new social battle royale game",
+        "ru": "прими участие в королевской битве",
+        "be": "прымі ўдзел у каралеўскай бітве"
+    },
+    "attr-name7": {
+        "en": "Buy Now on Steam",
+        "ru": "Купить в Steam",
+        "be": "Купіць у Steam"
+    },
+    "attr-name8": {
+        "en": "what is 'SOS'?",
+        "ru": "о чем игра 'SOS'?",
+        "be": "пра што гульня 'SOS'?"
+    },
+    "attr-name9": {
+        "en": "social battle</br>royale game",
+        "ru": "лето & море</br>боль & кровь",
+        "be": "лета & мора</br>кроў & боль"
+    },
+    "attr-name10": {
+        "en": "Each round, <span>you</span> and <span>15</span> other contestants compete to escape a deadly island filled with monsters. The trick is: <span>three</span> people can survive. Will you run solo or form friendships with others to escape?",
+        "ru": "В каждом раунде <span>16</span> человек пытаются эвакуироваться со смертельно опасного острова. Только <span>трое</span> смогут выжить. Будешь действовать в одиночку или ты предпочитаешь командную тактику?",
+        "be": "У кожным раунде <span>16</span> чалавек спрабуюць эвакуявацца са смяротна небяспечнага вострава. Толькі <span>трое</span> змогуць выжыць. Будзеш дзейнічаць у адзіночку ці ты аддаеш перавагу камандную тактыку?",
+    },
+    "attr-name11": {
+        "en": "Making the right decisions could be the difference between <span>life and death</span>.",
+        "ru": "Принятие верного решения увеличит твои шансы на <span>спасение</span>.",
+        "be": "Прыняцце верного рашэння павялічыць твае шанцы на <span>спасение</span>."
+    },
+    "attr-name12": {
+        "en": "what's so special?",
+        "ru": "в чём особенность игры?",
+        "be": "у чым асаблівасць гульні?"
+    },
+    "attr-name13": {
+        "en": "features",
+        "ru": "твои цели",
+        "be": "твае мэты"
+    },
+    "attr-name14": {
+        "en": "<div></div> survive at all costs",
+        "ru": "<div></div> выживи любой ценой",
+        "be": "<div></div> выжыць любой цаной"
+    },
+    "attr-name15": {
+        "en": "You have 30 minutes to find a relic, signal for extraction, and grab one of three spots on the rescue chopper.",
+        "ru": "У тебя есть 30 минут, чтобы найти реликвию, подать сигнал к эвакуации и занять одно из трех мест на вертолете.",
+        "be": "У цябе ёсць 30 хвілін, каб знайсці рэліквію, падаць сігнал да эвакуацыі i заняць 1 з 3 месцаў на верталёце."
+    },
+    "attr-name16": {
+        "en": "<div></div> get rid of the enemies",
+        "ru": "<div></div> избавься от врагов",
+        "be": "<div></div> пазбавіцца ад ворагаў"
+    },
+    "attr-name17": {
+        "en": "Keep your eyes open for footprints and other indicators that help you stalk your enemies.",
+        "ru": "Обращай внимание на следы и другие зацепки, с помощью которых можно выслеживать своих врагов.",
+        "be": "Звяртай увагу на сляды i іншыя зачэпкі, з дапамогай якіх можна высочваць сваіх ворагаў."
+    },
+    "attr-name18": {
+        "en": "<div></div> impress the audience",
+        "ru": "<div></div> удиви аудиторию",
+        "be": "<div></div> здзівіце аўдыторыю"
+    },
+    "attr-name19": {
+        "en": "You thought you were signing up for a reality TV show, but the network has decided it would be much more entertaining for their audience to watch you fight to the death instead.",
+        "ru": "Ты думал, что подписался на участие в реалити-шоу, но телеканал решил, что их аудитории будет гораздо интереснее наблюдать за тем, как ты вместо этого сражаешься насмерть.",
+        "be": "Ты думаў, што падпісаўся на ўдзел у рэаліці-шоў, але тэлеканал вырашыў, што іх аўдыторыі будзе значна цікавей назіраць за тым, як ты замест гэтага змагаешся да смерці."
+    },
+    "attr-name20": {
+        "en": "can my computer run this game?",
+        "ru": "характеристики компьютера",
+        "be": "характарыстыкі кампутара"
+    },
+    "attr-name21": {
+        "en": "system requirements",
+        "ru": "системные требования",
+        "be": "сістэмныя патрабаванні"
+    },
+    "attr-name22": {
+        "en": "os:",
+        "ru": "ос:",
+        "be": "ас:"
+    },
+    "attr-name23": {
+        "en": "Windows 7 64-bit only<br>(No OS X support)",
+        "ru": "Windows 7 64-bit<br>(на OS X не работает)",
+        "be": "Windows 7 64-bit<br>(на OS X не працуе)"
+    },
+    "attr-name24": {
+        "en": "processor:",
+        "ru": "процессор:",
+        "be": "працэсар:"
+    },
+    "attr-name25": {
+        "en": "Intel Core 2 Duo @ 2.4 GHZ<br>or AMD Athlon X2 @ 2.8 GHZ",
+        "ru": "Intel Core 2 Duo @ 2.4 ГГЦ<br>или AMD Athlon X2 @ 2.8 ГГЦ",
+        "be": "Intel Core 2 Duo @ 2.4 ГГЦ<br>або AMD Athlon X2 @ 2.8 ГГЦ"
+    },
+    "attr-name26": {
+        "en": "memory:",
+        "ru": "память:",
+        "be": "памяць:"
+    },
+    "attr-name27": {
+        "en": "8GB RAM",
+        "ru": "8ГБ ОП",
+        "be": "8ГБ АП"
+    },
+    "attr-name28": {
+        "en": "storage:",
+        "ru": "накопитель:",
+        "be": "накапляльнік:"
+    },
+    "attr-name29": {
+        "en": "8GB available space",
+        "ru": "8ГБ свободного места",
+        "be": "8ГБ вольнага месца"
+    },
+    "attr-name30": {
+        "en": "graphics:",
+        "ru": "видеокарта:",
+        "be": "відэакарта:"
+    },
+    "attr-name31": {
+        "en": "NVIDIA GeForce GTX 660 2GB <br>or AMD Radeon HD 7850 2GB DirectX11 (Shader Model 5)",
+        "ru": "NVIDIA GeForce GTX 660 2GB <br>или AMD Radeon HD 7850 2GB DirectX11 (Модель Шейдера 5)",
+        "be": "NVIDIA GeForce GTX 660 2GB <br>або AMD Radeon HD 7850 2GB DirectX11 (Мадэль Шэйдара 5)"
+    },
+    "attr-name32": {
+        "en": "E. Lahti",
+        "ru": "Э. Лахти",
+        "be": "Э. Лахці"
+    },
+    "attr-name33": {
+        "en": "Streamer",
+        "ru": "Стример",
+        "be": "Стрымер"
+    },
+    "attr-name34": {
+        "en": "&#34The next big thing in the world of streaming and survival games.&#34",
+        "ru": "&#34Важное событие для игровой и стриминговой индустрии.&#34",
+        "be": "&#34Важнае падзея для гульнявой i стрымінгавай індустрыі.&#34"
+    },
+    "attr-name35": {
+        "en": "October 18, 2018",
+        "ru": "18 октября 2018 года",
+        "be": "18 кастрычніка 2018 года"
+    },
+    "attr-name36": {
+        "en": "J. Griffin",
+        "ru": "Дж. Гриффин",
+        "be": "Дж. Грыфін"
+    },
+    "attr-name37": {
+        "en": "Designer",
+        "ru": "Дизайнер",
+        "be": "Дызайнер"
+    },
+    "attr-name38": {
+        "en": "&#34One of my gaming highlights of the year.&#34",
+        "ru": "&#34Один из моих фаворитов на звание игры года.&#34",
+        "be": "&#34Адзiн з маіх фаварытаў на званне гульні года.&#34"
+    },
+    "attr-name39": {
+        "en": "December 21, 2018",
+        "ru": "21 декабря 2018 года",
+        "be": "21 снежня 2018 года"
+    },
+    "attr-name40": {
+        "en": "A. Williams",
+        "ru": "А. Уильямс",
+        "be": "А. Уільямс"
+    },
+    "attr-name41": {
+        "en": "Musician",
+        "ru": "Музыкант",
+        "be": "Музыкант"
+    },
+    "attr-name42": {
+        "en": "&#34Snoop Dogg playing the wildly entertaining 'SOS' is ridiculous.&#34",
+        "ru": "&#34Забавно наблюдать за тем, как Снуп Догг играет в 'SOS'.&#34",
+        "be": "&#34Пацешна назіраць за тым, як Снуп Догг гуляе ў гэтую гульню.&#34"
+    },
+    "attr-name43": {
+        "en": "December 24, 2018",
+        "ru": "24 декабря 2018 года",
+        "be": "24 снежня 2018 года"
+    },
+    "attr-name44": {
+        "en": "what people think?",
+        "ru": "отзывы людей",
+        "be": "водгукі людзей"
+    },
+    "attr-name45": {
+        "en": "press quotes",
+        "ru": "комментарии",
+        "be": "каментары"
+    },
+    "attr-name46": {
+        "en": "Our goal is to create a product and service that you're satisfied with and use it every day. This is why we're constantly working on our services to make it better every day and really listen to what our users have to say.",
+        "ru": "Наша цель - создать продукт, которым бы ты хотел пользоваться каждый день. Поэтому мы постоянно работаем над сервисами игры и реально прислушиваемся к тому, что говорят о 'SOS' наши клиенты.",
+        "be": "Наша мэта - стварыць прадукт, якім бы ты хацеў карыстацца кожны дзень. Таму мы пастаянна працуем над сэрвісамі гульні i рэальна прыслухоўваемся да таго, што кажуць пра 'SOS' нашы кліенты."
+    },
+    "attr-name47": {
+        "en": "want to stay in touch?",
+        "ru": "свежие новости",
+        "be": "апошнія навіны"
+    },
+    "attr-name48": {
+        "en": "newsletter subscribe",
+        "ru": "оставайся на связи",
+        "be": "заставайся на сувязі"
+    },
+    "attr-name49": {
+        "en": "In order to start receiving our news, all you have to do is enter your email address. Everything else will be taken care of by us. We will send you emails containing information about game. We don't spam.",
+        "ru": "Укажи свой адрес электронной почты, если хочешь получать актуальную информацию об игре, в том числе новости о грядущих обновлениях 'SOS'. Спам не рассылаем.",
+        "be": "Укажи свой адрас электроннай пошты, калі хочаш получать актуальную інфармацыю аб гульні, у тым ліку навіны аб будучых абнаўленнях 'SOS'. Спам не рассылаем."
+    },
+    "attr-name50": {
+        "en": "Subscribe now",
+        "ru": "Подписаться сейчас",
+        "be": "Падпісацца зараз"
+    },
+    "attr-name51": {
+        "en": "about",
+        "ru": "об игре",
+        "be": "аб гульні"
+    },
+    "attr-name52": {
+        "en": "game features",
+        "ru": "особенности",
+        "be": "асаблівасці"
+    },
+    "attr-name53": {
+        "en": "system requirements",
+        "ru": "сис. требования",
+        "be": "сіс. патрабаванні"
+    },
+    "attr-name54": {
+        "en": "quotes",
+        "ru": "комментарии",
+        "be": "каментар"
+    },
+    "attr-name55": {
+        "en": "© 2023 Outpost Games, Inc. Designed by",
+        "ru": "© 2023 Outpost Games, Inc. Автор макета",
+        "be": "© 2023 Outpost Games, Inc. Аўтар макета"
+    },
+    "attr-name56": {
+        "en": "Developed by",
+        "ru": "Разработчик сайта",
+        "be": "Распрацоўшчык сайта"
+    },
+    "attr-name57": {
+        "en": "privacy policy",
+        "ru": "приватность",
+        "be": "прыватнасць"
+    },
+    "attr-name58": {
+        "en": "terms of services",
+        "ru": "обслуживание",
+        "be": "абслугоўванне"
+    },
+    "attr-name59": {
+        "en": "code of conduct",
+        "ru": "кодекс этики",
+        "be": "кодэкс этыкі"
+    },
+    "attr-name60": {
+        "en": "website navigation",
+        "ru": "навигация по сайту",
+        "be": "навігацыя па сайце"
+    },
+    "attr-name61": {
+        "en": "regulations",
+        "ru": "свод правил",
+        "be": "збор правілаў"
+    },
+};
